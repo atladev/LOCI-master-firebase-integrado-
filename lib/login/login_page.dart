62b05aaeccd99541a43ee0f216a011efd2e49b7d
Atla Developers <79058104+atladev@users.noreply.github.com>
2021-05-18 05:02:52 -0300

    2

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -12,6 +12,8 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
+
+
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:loci/core/core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
 WidgetsFlutterBinding.ensureInitialized();
 await Firebase.initializeApp();
+FirebaseAuth auth = FirebaseAuth.instance;
+UserCredential userCredential = await FirebaseAuth.instance.signInAnonymously();
   runApp(AppWidget());
 }
+
+
